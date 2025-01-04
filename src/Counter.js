7e744a86e4388554a20ec8ab4fd4abf9a4355fe1
frankzhang <frank.zhang.sa.au@gmail.com>
2025-01-04 10:06:14 +1000

    1 Create Counter Component

---
+++ src/Counter.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const Counter = ({ initialCount }) => {
+    const [count, setCount] = useState(initialCount);
+    const increment = () => {
+        setCount((prev) => prev + 1);
+    };
+    const decrement = () => {
+        setCount((prev) => prev - 1);
+    };
+    const reStart = () => {
+        setCount(0);
+    };
+    const switchSigns = () => {
+        setCount((prev) => prev * (-1));
+    };
+    return (
+        <div>
+            <h1>
+                Count: <h3>{count}</h3>
+            </h1>
+            <div>
+                <button onClick={increment}>Increment</button>
+                <button onClick={decrement}>Decrement</button>
+                <button onClick={reStart}>Restart</button>
+                <button onClick={switchSigns}>Switch Signs</button>
+            </div>
+        </div>
+    )
+}
+
+export default Counter
